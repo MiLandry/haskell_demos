a0399f81531487af21515b0804b54a49d0ab3918
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-11-25 16:08:04 -0500

    more details to functions

--- 02functions.hs
+++ 02functions.hs
@@ -1,7 +1,19 @@
---signature, good practice but doesn't actually do anything, haskell can infer
-return5 :: Integer
+--a basic function, it takes no arguments, just returns 5
 return5 = 5
 
 
+--a function with an optional , signature, good practice but doesn't actually do anything, haskell can infer such things 
+return6 :: Integer
+return6 = 6
+
+
+--a function that takes an argument.  Note the total lack of parens(), either during function implementation or call.
+doubleMe x = x + x
+
+
+--a function call along with a type safety line, which is really only for the coders benefit.
 d :: Integer
 d = return5
+
+--calling a function that takes an argument
+e = doubleMe 4
