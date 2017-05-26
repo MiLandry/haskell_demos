72271702d3623fbbb316ec9427c36c1acc921b5f
Michael-Landry <michael.landry@turner.com>
2017-05-26 15:35:08 -0400

    sanotih

--- lists-and-data-structures/aTuples.hs
+++ lists-and-data-structures/aTuples.hs
@@ -1,5 +1,10 @@
--- tuples are like lists.  But they have a fixed size, and they don't have to contain the same type of element
--- they use parens
+--haskell only has 2 main data structures, so tuples have to do what lists can't
+-- tuples are like lists in that they group stuff together. Now for differences:
+-- Fixed size (immutable)
+-- can store different types 
+
+
+-- you know you are using tuples because they use parens
 
 d = fst ("foo",2)
 
